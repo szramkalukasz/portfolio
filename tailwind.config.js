09537f5d5d7497bf34955670afa47557c300610f
szramkalukasz <lukasz_szramka@wp.pl>
2024-03-03 22:38:55 +0100

    Add styles for  after hours and contact on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       },
       boxShadow: {
         innerlight: "inset 0 2px 0 0 rgba(255,255,255,0.15)",
+        innerlightmini: "inset 0 1px 0 0 rgba(255,255,255,0.15)",
       },
     },
   },
